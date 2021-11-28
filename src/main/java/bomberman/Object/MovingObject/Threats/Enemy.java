1023cbc15933d2ce11dd56bcd75cf0ef14c1598b
iostream13 <20020013@vnu.edu.vn>
2021-11-28 12:28:27 +0700

    bot kẹt không chạy được trái phải

--- src/main/java/bomberman/Object/MovingObject/Threats/Enemy.java
+++ src/main/java/bomberman/Object/MovingObject/Threats/Enemy.java
@@ -4,9 +4,21 @@ import bomberman.Object.MovingObject.MovingObject;
 import bomberman.PvB_GamePlay;
 import javafx.util.Pair;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Enemy extends MovingObject {
+
+    /**
+     * Trạng thái đường đi của object.
+     */
+    private int[][] state = new int[50][50];
+    public boolean ok = false;
+    public boolean toRight = false;
+    public boolean toLeft = false;
+    public boolean toUp = false;
+    public boolean toDown = false;
 
     /**
      * Thời gian di chuyển theo hướng hiện tại
@@ -37,6 +49,140 @@ public class Enemy extends MovingObject {
         super(x, y, 35, 35); // Kích thước mặc định
     }
 
+    public int[][] getState() {
+        return state;
+    }
+
+    public void setState(int[][] state) {
+        this.state = state;
+    }
+
+    public void findBestWay(int xEnemy, int yEnemy, int xPlayer, int yPlayer) {
+        boolean[][] dx = new boolean[110][110];
+        Pair<Integer, Integer>[][] tr = new Pair[110][110];
+        int[] c = {0, 0, -1, 1};
+        int[] d = {1, -1, 0, 0};
+        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
+        Pair<Integer, Integer> p;
+        p = new Pair<>(yEnemy, xEnemy);
+        dx[yEnemy][xEnemy] = true;
+        q.add(p);
+        while (q.size() > 0) {
+            p = q.element();
+            q.remove();
+            int y = p.getKey();
+            int x = p.getValue();
+            if (x == xPlayer && y == yPlayer) {
+                for (int i = 1; i < PvB_GamePlay.map.numberOfRow; i++) {
+                    for (int j = 1; j < PvB_GamePlay.map.numberOfColumn; j++) {
+                        state[i][j] = 0;
+                    }
+                }
+                int tmpX = xPlayer;
+                int tmpY = yPlayer;
+                while (tmpX != xEnemy || tmpY != yEnemy) {
+                    state[tmpY][tmpX] = 1;
+                    p = tr[tmpY][tmpX];
+                    tmpY = p.getKey();
+                    tmpX = p.getValue();
+                }
+                state[tmpY][tmpX] = 1;
+                /*for (int i = 0; i <= 3; i++) {
+                    int newX = xEnemy + c[i];
+                    int newY = yEnemy + d[i];
+                    if (newX < 1 || newX >= PvB_GamePlay.map.numberOfColumn || newY < 1 || newY > PvB_GamePlay.map.numberOfRow) {
+                        continue;
+                    }
+                    if (state[newY][newX] == 1) {
+                        if (i == 0) {
+                            toDown = true;
+                            toUp = false;
+                        }
+                        else {
+                            if (i == 1) {
+                                toUp = true;
+                                toDown = false;
+                            }
+                        }
+                        if (i == 2) {
+                            toLeft = true;
+                            toRight = false;
+                        }
+                        else {
+                            if (i == 3) {
+                                toRight = true;
+                                toLeft = false;
+                            }
+                        }
+                    }
+                }*/
+                /*for (int i = 0; i < PvB_GamePlay.map.numberOfRow; i++) {
+                    for (int j = 0; j < PvB_GamePlay.map.numberOfColumn; j++) {
+                        System.out.print(state[i][j]);
+                        System.out.print(' ');
+                    }
+                    System.out.print("\n");
+                }*/
+                ok = true;
+                return;
+            }
+            p = new Pair<>(y, x);
+            for (int i = 0; i <= 3; i++) {
+                int newX = x + c[i];
+                int newY = y + d[i];
+                if (newX < 0 || newX >= PvB_GamePlay.map.numberOfColumn || newY < 0 || newY > PvB_GamePlay.map.numberOfRow) {
+                    continue;
+                }
+                if (!PvB_GamePlay.map.isBlockCell(newY, newX) && !dx[newY][newX]) {
+                    dx[newY][newX] = true;
+                    tr[newY][newX] = p;
+                    Pair <Integer, Integer> pp = new Pair<>(newY, newX);
+                    q.add(pp);
+                }
+            }
+        }
+        ok = false;
+    }
+
+    public void setWay(int xEnemy, int yEnemy) {
+        int[] c = {0, 0, -1, 1};
+        int[] d = {1, -1, 0, 0};
+        for (int i = 0; i <= 3; i++) {
+            int newX = xEnemy + c[i];
+            int newY = yEnemy + d[i];
+            if (newX < 1 || newX >= PvB_GamePlay.map.numberOfColumn || newY < 1 || newY > PvB_GamePlay.map.numberOfRow) {
+                continue;
+            }
+            if (state[newY][newX] == 1) {
+                if (i == 0) {
+                    toDown = true;
+                    toUp = false;
+                    toLeft = false;
+                    toRight = false;
+                }
+                else {
+                    if (i == 1) {
+                        toUp = true;
+                        toDown = false;
+                        toLeft = false;
+                        toRight = false;
+                    } else if (i == 2) {
+                        toUp = false;
+                        toDown = false;
+                        toLeft = true;
+                        toRight = false;
+                    } else {
+                            toUp = false;
+                            toDown = false;
+                            toRight = true;
+                            toLeft = false;
+                    }
+                }
+
+
+            }
+        }
+    }
     @Override
     public void move() {
         if (System.nanoTime() - startTime >= duration) {
